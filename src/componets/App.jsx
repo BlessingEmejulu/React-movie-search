9aa73ee8b66ff515e959378b030f5c05b256097c
BlessingEmejulu <blessingnneamaka57@gmail.com>
2024-09-15 07:41:33 -0700

    first commit

---
+++ src/componets/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchMovie from "./SearchMovie.jsx";
+
+export default function App() {
+    return (
+        <div className="container">
+            <h1 className="title">React Movie Search</h1>
+            <SearchMovie />
+        </div>
+    )
+}
---
+++ src/componets/CardList.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+// export default function CardList() {
+//     return (
+//         <div className="card-list">
+//                {movies.map(movie => movie.title)}
+//             </div>    
+//     );
+// }
