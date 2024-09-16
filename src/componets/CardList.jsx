655385a1206eb0948ca74a87aefdf3fafc333837
BlessingEmejulu <blessingnneamaka57@gmail.com>
2024-09-15 22:31:29 -0700

    cardlist component deleted

--- src/componets/CardList.jsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// export default function CardList() {
-//     return (
-//         <div className="card-list">
-//                {movies.map(movie => movie.title)}
-//             </div>    
-//     );
-// }
--- src/componets/MovieCard.jsx
+++ src/componets/MovieCard.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function MovieCard() {
     return (
         <div className="card"  >
-        <img className="card--image"
-        src= ""/>
+          <img className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + ' poster'}
+                />
         <div className="card--content">
         <h3 className="card--title">{movie.title}</h3>
         <p><small>RELEASE DATE: {movie.release_date}</small></p>
